930efa99d5d69fe8b017782ee63708c2ccb7bb12
Megudora11 <megudora11oishi@outlook.com>
2021-04-30 23:20:35 +0900

    Update Input.java

--- Input.java
+++ Input.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 class Input {
     Scanner scanner = new Scanner(System.in);
 
-    public int inputNum(){
+    public int inputNum(){                      //メニュー選択
         System.out.print("Menu Number : ");
         int num = scanner.nextInt();
         return num;
     }
     
-    public int inputOne(){
+    public int inputOne(){                      //数字を1つだけ入力
         System.out.print("Input Number : ");
         int num = scanner.nextInt();
         return num;
     }
 
-    public int[] inputTwo(){
+    public int[] inputTwo(){                    //数字を2つ入力
         System.out.print("Input Number(A) : ");
         int num1 = scanner.nextInt();
         System.out.print("Input Number(B) : ");
