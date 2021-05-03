391d71345dfd5e82cfae25f674367df789cb3242
Megudora11 <megudora11oishi@outlook.com>
2021-05-03 22:48:51 +0900

    Menu Update

--- Main.java
+++ Main.java
@@ -1,32 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi! This Tool name is Mathematics.");
+        System.out.println("  -------------------\n"+
+                           " / Mathematic Tool /\n"+
+                           "------------------");
         
         Menu menu = new Menu();
-        Input nInput = new Input();
-        Explain explain = new Explain();
-
-        menu.printMenu1();
-        int num = nInput.inputNum();
-        System.out.print("Your choice is ");
-        explain.printText(num);
-
-        switch (num){
-            case 0:
-                System.out.println("Finish. Bye!");
-                break;
-            case 1:
-                Fact nFact = new Fact();
-                nFact.nFact();
-                break;
-            case 2:
-                Euclid nEuclid = new Euclid();
-                nEuclid.nEuclid();
-                break;
-            case 3:
-                Collatz nCollatz = new Collatz();
-                nCollatz.nCollatz();
-                break;
-        }
+        menu.Menu();
     }
 }
--- Menu.java
+++ Menu.java
@@ -1,4 +1,33 @@
 class Menu {
+    void Menu(){
+    Input nInput = new Input();
+    Explain explain = new Explain();
+
+    this.printMenu1();
+    int num = nInput.inputNum();
+    System.out.print("Your choice is ");
+    explain.printText(num);
+
+    switch(num){
+            case 0:
+                System.out.println("Finish. Bye!");
+                break;
+            case 1:
+                Fact nFact = new Fact();
+                nFact.nFact();
+                break;
+            case 2:
+                Euclid nEuclid = new Euclid();
+                nEuclid.nEuclid();
+                break;
+            case 3:
+                Collatz nCollatz = new Collatz();
+                nCollatz.nCollatz();
+                break;
+        }
+    }
+    
+
     public void printMenu1(){
         System.out.println("[Menu]\n" +
                 "1:Prime Factorization\n" + // 素因数分解
